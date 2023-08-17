5ba2cbce66840a77fcbc5090dcb2d9d70b6c40ac
Ezana Tareke <etareke25@gmail.com>
2023-08-17 13:47:39 +0100

    game now takes case insensitive input game function has a loop which calls playRound function 5 times game function also returns the winner of the 5 rounds

--- code.js
+++ code.js
@@ -12,38 +12,66 @@ function getComputerChoice() {
     else return "Scissors"
 }
 
+// win and loss counters to decide who wins each round of 5
+
+let winCounter = 0;
+let lossCounter = 0;
+
+function firstUpperCase(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+}
+
+// plays one round of the game and returns the winner of the round
 
 function playRound(playerSelection, computerSelection) {
 
 
-    if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    if (firstUpperCase(playerSelection) === "Rock" && computerSelection === "Scissors") {
+        winCounter++
         return "You Win! Rock beats Scissors";
     }  
-    else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    else if (firstUpperCase(playerSelection) === "Paper" && computerSelection === "Rock") {
+        winCounter++
         return "You Win! Paper beats Rock";
     }
-    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    else if (firstUpperCase(playerSelection) === "Scissors" && computerSelection === "Paper") {
+        winCounter++
         return "You Win! Scissors beats Paper";
     }
-    else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    else if (firstUpperCase(playerSelection) === "Scissors" && computerSelection === "Rock") {
+        lossCounter++
         return "You Lose! Rock beats Scissors";
     }
-    else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    else if (firstUpperCase(playerSelection) === "Rock" && computerSelection === "Paper") {
+        lossCounter++
         return "You Lose! Paper beats Rock";
     }
-    else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    else if (firstUpperCase(playerSelection) === "Paper" && computerSelection === "Scissors") {
+        lossCounter++
         return "You Lose! Scissors beats Paper";
     }
-    else if (playerSelection === computerSelection) {
+    else if (firstUpperCase(playerSelection) === computerSelection) {
         return "It's a Tie!"
     }
     else return "Please pick Rock, Paper or Scissors";
          
 }
 
+// game function loops the game 5 times and outputs who has won the most rounds
 
 function game() {
+
+    for (i=1; i < 6; i++) {
     let choose = prompt("Pick Rock, Paper or Scissors")
-    console.log(playRound(choose, getComputerChoice()));
+    alert(playRound(choose, getComputerChoice()));
+    }
+
+    if (winCounter > lossCounter) {
+        alert("Congrats you have won the game");
+    }
+    else if (lossCounter > winCounter) {
+        alert("Sorry you have lost the game!")
+    }
+    else alert("Theres no winner this time :/")
 }
 
